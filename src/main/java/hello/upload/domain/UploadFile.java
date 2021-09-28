1d12ee5b40e2f7fb6749ecbf237135bc81e89171
JongGurlHan <gojkhan13@gmail.com>
2021-09-28 19:13:58 +0900

    0928 파일업로드, 다운로드-2

--- src/main/java/hello/upload/domain/UploadFile.java
+++ src/main/java/hello/upload/domain/UploadFile.java
@@ -5,8 +5,8 @@ import lombok.Data;
 @Data
 public class UploadFile {
 
-    private String uploadFileName; //업로드한 파일명
-    private String storeFileName; //시스템에 저장한 파일명 사용자 모두 같은파일명으로 업로드하면 db에 파일이 덮어질 수 있어서 uuid같은걸 만들어서 안겹치게 만들어야
+    private String uploadFileName; //고객이 업로드한 파일명
+    private String storeFileName; //시스템에 저장한 파일명 ,사용자 모두 같은파일명으로 업로드하면 db에 파일이 덮어질 수 있어서 uuid같은걸 만들어서 안겹치게 만들어야
 
 
 
